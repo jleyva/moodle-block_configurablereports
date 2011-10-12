6648ea166768fc905d9a9d112a63fce1d3752b90
Juan Leyva <juanleyvadelgado@gmail.com>
2011-10-13 00:39:43 +0200

    Version changed

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
-    $plugin->version = 2011040100;
+    $plugin->version = 2011040101;
     // Moodle version required
     $plugin->requires = 2010112400;
     $plugin->maturity = MATURITY_STABLE;
