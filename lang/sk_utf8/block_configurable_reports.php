4815916204c220c52e65c9f678a91ac108a398c1
Juan Leyva <juanleyvadelgado@gmail.com>
2011-10-12 09:08:42 +0200

    Adding sk lang/sk_utf8/block_configurable_reports.php

---
+++ lang/sk_utf8/block_configurable_reports.php
@@ -0,0 +1,191 @@
+﻿<?php
+
+$string['blockname'] = "Vlastné správy";
+$string['report_courses'] = "Správy kurzov";
+$string['report_users'] = "Správy užívateľov";
+$string['report_sql'] = "SQL Správy";
+$string['managereports'] = "Riadiť správy";
+
+$string['report'] = "Správa";
+$string['reports'] = "Správy";
+
+$string['columns'] = "Sĺpce";
+$string['conditions'] = "Podmienky";
+$string['permissions'] = "Oprávnenia";
+$string['plot'] = "Plot - Grafy";
+$string['filters'] = "Filtre";
+$string['calcs'] = "Kalkulácie";
+$string['ordering'] = "Poradie";
+$string['customsql'] = "Prispôsobené SQL";
+$string['addreport'] = "Pridať správu";
+$string['type'] = "Typ správy";
+$string['columncalculations'] = "Kalkulácie stĺpcov";
+$string['newreport'] = "Nová správa";
+$string['column'] = "Stĺpec";
+$string['confirmdeletereport'] = "Ste si istí, že chcete vymazať túto správu?";
+$string['noreportsavailable'] = "Nie sú dostupné žiadne správy";
+$string['downloadreport'] = "Stiahnuť správu";
+
+$string['configurable_reports:manageownreports'] = "Riadiť vlastné správy";
+$string['configurable_reports:managereports'] = "Riadiť správy";
+$string['configurable_reports:managesqlreports'] = "Riadiť SQL správy";
+$string['configurable_reports:viewreports'] = "Zobraziť správy";
+
+$string['exportoptions'] = "Možnosti exportu";
+$string['field'] = "Pole";
+
+// Report form
+$string['typeofreport'] = "Typ správy";
+$string['enablejsordering'] = "Umožniť JavaScript poradie";
+$string['enablejspagination'] = "Umožniť JavaScript stránkovanie";
+$string['export_ods'] = "Exportovať do ODS formátu";
+$string['export_xls'] = "Exportovať do XLS formátu";
+$string['viewreport'] = "Zobraziť správu";
+$string['norecordsfound'] = "Žiadne záznamy neboli nájdené";
+
+// Columns
+$string['column'] = "Stĺpec";
+$string['nocolumnsyet'] = "Ešte neboli zvolené stĺpce";
+$string['tablealign'] = "Zarovnanie tabuľky";
+$string['tablecellspacing'] = "Rozloženie buniek tabuľky";
+$string['tablecellpadding'] = "Rozloženie buniek tabuľky";
+$string['tableclass'] = "Trieda tabuľky";
+$string['tablewidth'] = "Šírka tabuľky";
+$string['cellalign'] = "Zarovnanie bunky";
+$string['cellwrap'] = "Obal bunky";
+$string['cellsize'] = "Veľkosť bunky";
+
+
+// Conditions
+$string['conditionexpr'] = "Podmienka";
+$string['conditionexprhelp'] = "Vložte platnú podmienku, napr.: (c1 a c2) alebo (c4 a c3)";
+$string['noconditionsyet'] = "Ešte neboli zvolené podmienky";
+$string['operator'] = "Operátor";
+$string['value'] = "Hodnota";
+
+// Filter
+$string['filter'] = "Filter";
+$string['nofilteryet'] = "Ešte nebol zvolený filter";
+$string['courses'] = "Kurzy";
+$string['nofiltersyet'] = "Ešte neboli zvolené filtre";
+
+// Calcs
+$string['nocalcsyet'] = "Ešte neboli zvolené kalkulácie";
+
+// Plot
+$string['noplotyet'] = "Ešte neboli zvolené grafy";
+
+// Permissions
+
+$string['nopermissionsyet'] = "Ešte neboli zvolené práva";
+
+// Ordering
+
+$string['noorderingyet'] = "Ešte nebolo zvolené poradie";
+$string['userfieldorder'] = "Poradie užívateľského poľa";
+
+// Plugins
+$string['coursefield'] = "Pole kurzu";
+$string['ccoursefield'] = "Podmienka poľa kurzu";
+$string['roleusersn'] = "Počet užívateľov, ktorí majú oprávnenie...";
+$string['coursecategory'] = "Kurz v kategórii";
+$string['filtercourses'] = "Kurzy";
+$string['filtercourses_summary'] = "Tento filter zobrazí zoznam  kurzov. Iba jeden kurz môže byť simultatívne zvolený";
+$string['roleincourse'] = "Užívatelia s vybraným oprávnením v súčastnej správe z kurzu";
+$string['reportscapabilities'] = "Možnosti správy";
+$string['reportscapabilities_summary'] = "Užívatelia s povolenou možnosťou moodle/site:viewreports";
+$string['sum'] = "Súčet";
+$string['max'] = "Maximum";
+$string['min'] = "Minimum";
+$string['average'] = "Priemer";
+$string['pie'] = "Koláč";
+$string['piesummary'] = "Graf v tvare Koláča";
+$string['pieareaname'] = "Názov";
+$string['pieareavalue'] = "Hodnota";
+$string['piesummary'] = "Graf v tvare Koláča";
+
+$string['anyone'] = "Niekto";
+$string['anyone_summary'] = "Všetci užívatelia v rámci kurzu môžu vidieť túto správu";
+
+$string['currentuserfinalgrade'] = "Súčasné konečné hodnotenie užívateľa v kurze";
+
+$string['currentuserfinalgrade_summary'] = "Tento stĺpec zobrazí konečné hodnotenie užívateľa v kurze";
+$string['userfield'] = "Pole profilu užívateľa";
+
+$string['cuserfield'] = "Podmienka poľa užívateľa";
+$string['direction'] = "Smer";
+
+$string['courseparent'] = "Kurzy, ktoré majú parameter";
+$string['coursechild'] = "Kurzy, ktoré sú podradené";
+
+$string['currentusercourses'] = "Kurzy, do ktorých je užívateľ momentálne prihlásený";
+$string['currentusercourses_summary'] = "Zoznam súčasných kurzov užívateľa (len viditeľné kurzy)";
+$string['currentreportcourse'] = "Súčasný prehľad kurzu";
+$string['currentreportcourse_summary'] = "Kurz, pre ktorý bola správa generovaná";
+
+$string['coursefieldorder'] = "Poradie poľa kurzu";
+
+$string['fcoursefield'] = "Filter poľa kurzu";
+$string['usersincoursereport'] = "Užívateľ v súčasnej správe kurzu";
+
+$string['groupvalues'] = "Zoskupiť rovnaké hodnoty (súčet)";
+$string['fuserfield'] = "Filter poľa užívateľa";
+
+$string['module'] = "Modul";
+
+$string['usersincurrentcourse'] = "Užívatelia v súčasnej správe kurzu";
+$string['usersincurrentcourse_summary'] = "Užívatelia so zvoleným oprávnením v správe kurzu";
+
+$string['usermodoutline'] = "Prehľad štatistiky modulu užívateľa";
+$string['donotshowtime'] = "Nezobrazovať dátum";
+$string['usermodactions'] = "Akcie modulu užívateľa";
+
+$string['currentuser'] = "Súčasný užívateľ";
+$string['currentuser_summary'] = "Užívateľ, ktorý vidí správu";
+
+$string['puserfield'] = "Hodnota poľa užívateľa";
+$string['puserfield_summary'] = "Užívateľ s vybranou hodnotou vo zvolenom poli";
+
+$string['startendtime'] = "Začiatok/Koniec filtrovania dátumu";
+$string['starttime'] = "Začiatočný dátum";
+$string['endtime'] = "Konečný dátum";
+
+$string['template'] = "Vzor";
+$string['availablemarks'] = "Dostupné označenia";
+$string['header'] = "Vrchná časť správy";
+$string['footer'] = "Spodná časť správy";
+$string['templaterecord'] = "Vzor záznamu";
+$string['querysql'] = "SQL Query";
+$string['filterstartendtime_summary'] = "Začiatok/Koniec filtrovania dátumu";
+
+$string['pagination'] = "Stránkovanie";
+$string['disabled'] = "Nepovolené";
+$string['enabled'] = "Povolené";
+
+$string['reportcolumn'] = "Iné stĺpce správy";
+
+$string['reporttable'] = "Tabuľka správy";
+$string['columnandcellproperties'] = "Vlastnosti stĺpca a bunky";
+$string['componenthelp'] = "Pomoc pre komponent";
+
+$string['badsize'] = 'Nesprávna veľkosť, hodnota musí byť v &#37; alebo px';
+$string['badtablewidth'] = 'Nesprávna šírka, hodnota musí byť v &#37; alebo v absolútnej hodnote';
+$string['missingcolumn'] = "Nesprávny stĺpec";
+$string['error_operator'] = "Operátor nie je povolený";
+
+$string['error_field'] = "Pole nie je povolené";
+$string['error_value_expected_integer'] = "Očakávaná hodnota je celé číslo";
+$string['badconditionexpr'] = "Nesprávny výraz hodnoty";
+
+$string['notallowedwords'] = "Slová nie sú povolené";
+$string['nosemicolon'] = "Bez bodkočiarky (;)";
+$string['noexplicitprefix'] = "Bez explicitnej predpony";
+$string['queryfailed'] = "Query bola nesprávna";
+$string['norowsreturned'] = "Žiadne záznamy na zobrazenie";
+
+$string['listofsqlreports'] = '<a href=\"http://docs.moodle.org/en/ad-hoc_contributed_reports\" target=\"_blank\">Zoznam príspevkov SQL správ</a>';
+
+$string['usersincoursereport_summary'] = "Nejaký užívateľ v súčasnej správe kuzru";
+$string[''] = "";
+
+?>
