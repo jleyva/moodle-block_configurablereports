3494b40ff08222cf9ce7ab8ddcf858d3962f7f24
Juan Leyva <juanleyvadelgado@gmail.com>
2011-10-12 09:05:14 +0200

    Adding missing lang dir

---
+++ lang/es_utf8/block_configurable_reports.php
@@ -0,0 +1,242 @@
+<?php
+
+$string['blockname'] = "Informes personalizados";
+$string['report_courses'] = "Informe de cursos";
+$string['report_users'] = "Informes de usuarios";
+$string['report_sql'] = "Informe SQL";
+$string['managereports'] = "Gestionar informes";
+
+$string['report'] = "Informe";
+$string['reports'] = "Informes";
+
+$string['columns'] = "Columnas";
+$string['conditions'] = "Condiciones";
+$string['permissions'] = "Permisos";
+$string['plot'] = "Gráficas";
+$string['filters'] = "Filtros";
+$string['calcs'] = "Cálculos";
+$string['ordering'] = "Ordenación";
+$string['customsql'] = "Sentencia SQL";
+$string['addreport'] = "Añadir informe";
+$string['type'] = "Tipo de informe";
+$string['columncalculations'] = "Cálculos sobre columnas";
+$string['newreport'] = "Nuevo informe";
+$string['column'] = "Columna";
+$string['confirmdeletereport'] = "¿Estás seguro de que quieres borrar este informe?";
+$string['noreportsavailable'] = "No hay informes disponibles";
+$string['downloadreport'] = "Descargar informe";
+
+$string['configurable_reports:manageownreports'] = "Gestionar mis informes";
+$string['configurable_reports:managereports'] = "Gestionar informes";
+$string['configurable_reports:managesqlreports'] = "Gestionar informes SQL";
+$string['configurable_reports:viewreports'] = "Ver informes";
+
+$string['exportoptions'] = "Opciones de exportación";
+$string['field'] = "Campo";
+
+// Report form
+$string['typeofreport'] = "Tipo de informe";
+$string['enablejsordering'] = "Habilitar ordenación mediante JavaScript";
+$string['enablejspagination'] = "Habilitar paginación mediante JavaScript";
+$string['export_ods'] = "Exportar en formato ODS";
+$string['export_xls'] = "Exportar en formato XLS";
+$string['viewreport'] = "Ver informe";
+$string['norecordsfound'] = "No se encontraron registros";
+
+// Columns
+$string['column'] = "Columna";
+$string['nocolumnsyet'] = "No hay columnas todavía";
+$string['tablealign'] = "Alineación de la tabla";
+$string['tablecellspacing'] = "Espacio entre celdas";
+$string['tablecellpadding'] = "Margen de las celdas";
+$string['tableclass'] = "Clase CSS para la tabla";
+$string['tablewidth'] = "Ancho de la tabla";
+$string['cellalign'] = "Alineación de la celda";
+$string['cellwrap'] = "¿Cortar texto en la celda?";
+$string['cellsize'] = "Tamaño de la celda";
+
+
+// Conditions
+$string['conditionexpr'] = "Condición lógica";
+$string['conditionexprhelp'] = "Teclee una condición lógica válida: (c1 and c2) or (c4 and c3)";
+$string['noconditionsyet'] = "No hay condiciones todavía";
+$string['operator'] = "Operador";
+$string['value'] = "Valor";
+
+// Filter
+$string['filter'] = "Filtro";
+$string['nofiltersyet'] = "No hay filtros todavía";
+$string['courses'] = "Cursos";
+
+// Calcs
+$string['nocalcsyet'] = "No hay cálculos todavía";
+
+// Plot
+$string['noplotyet'] = "No hay gráficas todavía";
+
+// Permissions
+
+$string['nopermissionsyet'] = "No hay permisos todavía";
+
+// Ordering
+
+$string['noorderingyet'] = "No hay ordenaciones todavía";
+$string['userfieldorder'] = "Ordenación por campo de usuario";
+
+// Plugins
+$string['coursefield'] = "Campo del curso";
+$string['ccoursefield'] = "Condición sobre campo del curso";
+$string['roleusersn'] = "Número de usuarios con el rol...";
+$string['coursecategory'] = "Curso en la categoría...";
+$string['filtercourses'] = "Cursos";
+$string['filtercourses_summary'] = "Este filtro muestra una lista de cursos. Sólo un curso puede seleccionado a la vez";
+$string['roleincourse'] = "User with the selected role/s in the current report course";
+$string['reportscapabilities'] = "Permisos de ver informes";
+$string['reportscapabilities_summary'] = "Usuarios con el permiso moodle/site:viewreports activado";
+$string['sum'] = "Suma";
+$string['max'] = "Máximo";
+$string['min'] = "Mínimo";
+$string['average'] = "Media";
+$string['pie'] = "Tarta";
+$string['piesummary'] = "Un gráfico de tarta";
+$string['pieareaname'] = "Nombre";
+$string['pieareavalue'] = "Valor";
+
+$string['anyone'] = "Cualquiera";
+$string['anyone_summary'] = "Cualquier usuario del Campus podrá ver este informe";
+
+$string['currentuserfinalgrade'] = "Nota final del usuario en el curso";
+
+$string['currentuserfinalgrade_summary'] = "Esta columna muestra la nota final en el curso de la fila actual del informe";
+$string['userfield'] = "Campo del perfil de usuario";
+
+$string['cuserfield'] = "Condición sobre campo del perfil de usuario";
+$string['direction'] = "Dirección";
+
+$string['courseparent'] = "Cursos cuyo padre es";
+$string['coursechild'] = "Cursos que son hijos de";
+
+$string['currentusercourses'] = "Cursos donde está matriculado el usuario";
+$string['currentusercourses_summary'] = "Un listado de los cursos donde está matriculado el usuario que visualiza el informe";
+$string['currentreportcourse'] = "Curso del informe";
+$string['currentreportcourse_summary'] = "El curso donde el informe ha sido creado";
+
+$string['coursefieldorder'] = "Ordenación sobre campo del curso";
+
+$string['fcoursefield'] = "Filtro sobre campo del curso";
+$string['usersincoursereport'] = "Cualquier usuario en el curso donde se ha creado el informe";
+
+$string['groupvalues'] = "Agrupar valores iguales (mediante suma)";
+$string['fuserfield'] = "Filtro sobre campo del perfil del usuario";
+
+$string['module'] = "Módulo";
+
+$string['usersincurrentcourse'] = "Usuarios en el curso donde se ha creado el informe";
+$string['usersincurrentcourse_summary'] = "Usuarios con los roles seleccionados en el curso del informe";
+
+$string['usermodoutline'] = "Estadísticas resumidas del usuario en el módulo";
+$string['donotshowtime'] = "No mostrar información de fechas";
+$string['usermodactions'] = "Acciones de usuario en el módulo";
+
+$string['currentuser'] = "Usuario actual";
+$string['currentuser_summary'] = "El usuario que está viendo el informe";
+
+$string['puserfield'] = "Valor en el perfil del usuario";
+$string['puserfield_summary'] = "Usuarios con el valor seleccionado en un campo de su perfil";
+
+$string['startendtime'] = "Filtro de fechas";
+$string['starttime'] = "Fecha de inicio";
+$string['endtime'] = "Fecha de fin";
+
+$string['template'] = "Plantilla";
+$string['availablemarks'] = "Marcas disponibles";
+$string['header'] = "Cabecera";
+$string['footer'] = "Pie";
+$string['templaterecord'] = "Plantilla para un registro";
+$string['querysql'] = "Consulta SQL";
+$string['filterstartendtime_summary'] = "Filtro de fecha de inicio / fin";
+
+$string['pagination'] = "Paginación";
+$string['disabled'] = "Deshabilitada";
+$string['enabled'] = "Habilitada";
+
+$string['reportcolumn'] = "Columna de otro informe";
+
+$string['reporttable'] = "Tabla del informe";
+$string['columnandcellproperties'] = "Propiedades de columna y celda";
+$string['componenthelp'] = "Ayuda del componente";
+
+$string['badsize'] = 'Tamaño incorrecto, debe estar en &#37; o px';
+$string['badtablewidth'] = 'Ancho incorrecto, debe estar en &#37; o en valor absoluto';
+$string['missingcolumn'] = "Se necesita al menos una columna";
+$string['error_operator'] = "Operador no permitido";
+
+$string['error_field'] = "Campo no permitido";
+$string['error_value_expected_integer'] = "Se esperaba un valor numérico";
+$string['badconditionexpr'] = "Expresión incorrecta";
+
+$string['notallowedwords'] = "Expresiones no permitidas";
+$string['nosemicolon'] = "No se permiten puntos y coma";
+$string['noexplicitprefix'] = "No se debe indicar el prefijo de tabla, usar prefix_ en su lugar";
+$string['queryfailed'] = "La consulta SQL falló";
+$string['norowsreturned'] = "La consulta no ha devuelto ningún resultado";
+
+$string['listofsqlreports'] = '<a href=\"http://docs.moodle.org/en/ad-hoc_contributed_reports\" target=\"_blank\">Listado de informes en SQL</a>';
+
+$string['usersincoursereport_summary'] = "Cualquier usuario en el curso donde se ha creado el informe";
+
+$string['printreport'] = 'Imprimir informe';
+
+$string['importreport'] = "Importar informe";
+$string['exportreport'] = "Exportar informe";
+
+$string['download'] = "Descargar";
+
+$string['report_timeline'] = 'Informe de linea temporal';
+$string['timeline'] = 'Linea temporal';
+$string['timemode'] = 'Modo temporal';
+$string['previousdays'] = 'Dias previos';
+$string['fixeddate'] = 'Fechas fijas';
+$string['previousstart'] = 'Comienzo hacia atrás';
+$string['previousend'] = 'Finalización hacia atrás';
+$string['forcemidnight'] = 'Forzar media noche';
+$string['timeinterval'] = 'Intervalo de tiempo';
+$string['date'] = 'Fecha';
+$string['dateformat'] = 'Formato de fecha';
+$string['customdateformat'] = 'Formato de fecha personalizado';
+$string['custom'] = 'Personalizado';
+
+$string['line'] = 'Gráfico de línea';
+
+$string['userstats'] = 'Estadísticas de usuaro';
+$string['stat'] = 'Estadísticas';
+$string['statslogins'] = 'Accesos a la plataforma';
+$string['activityview'] = 'Acciones de ver en actividades';
+$string['activitypost'] = 'Acciones de participar en actividades';
+
+$string['globalstatsshouldbeenabled'] = 'Las estadísticas del sitio han de estar habilitadas. Admin -> Servidor  -> Estadísticas';
+
+$string['xaxis'] = 'Eje X';
+$string['yaxis'] = 'Eje Y';
+$string['serieid'] = 'Columna de series';
+$string['groupseries'] = 'Agrupar series';
+$string['linesummary'] = 'Una gráfica lineal con múltiples series de datos';
+
+$string['coursestats'] = 'Estadísticas de curso';
+$string['statstotalenrolments'] = 'Matriculaciones totales';
+$string['statsactiveenrolments'] = 'Matriculaciones activas (última semana)';
+$string['youmustselectarole'] = 'Al menos has de seleccionar un rol';
+
+$string['report_categories'] = 'Informe de categorías';
+$string['categoryfield'] = 'Campo de categoría';
+$string['categoryfieldorder'] = 'Ordenación por campos de la Categoría';
+$string['categories'] = 'Categorías';
+$string['parentcategory'] = 'Categoría padre';
+$string['filtercategories'] = 'Filtro de categorías';
+$string['filtercategories_summary'] = 'Para filtrar por categoría';
+
+$string['includesubcats'] = 'Incluir subcategorías';
+
+$string['coursededicationtime'] = 'Tiempo dedicado al curso';
+
+?>
---
+++ lang/he_utf8/block_configurable_reports.php
@@ -0,0 +1,243 @@
+﻿<?php
+
+$string['blockname'] = "מעצב דוחות";
+$string['report_courses'] = "דוח קורסים";
+$string['report_users'] = "דוח משתמשים";
+$string['report_sql'] = "SQL דוח";
+$string['managereports'] = "נהל דוחות";
+
+$string['report'] = "דוח";
+$string['reports'] = "דוחות";
+
+$string['columns'] = "עמודות";
+$string['conditions'] = "התניות";
+$string['permissions'] = "הרשאות";
+$string['plot'] = "צייר גרפים";
+$string['filters'] = "פילטרים	";
+$string['calcs'] = "חישובים";
+$string['ordering'] = "סידור";
+$string['customsql'] = "מותאם SQL";
+$string['addreport'] = "הוסף דוח";
+$string['type'] = "סוג הדוח";
+$string['columncalculations'] = "חישובי עמודה";
+$string['newreport'] = "דוח חדש";
+$string['column'] = "עמודה";
+$string['confirmdeletereport'] = "האם אתה בטוח שברצונך למחוק את הדוח ?";
+$string['noreportsavailable'] = "לא נמצאו דוחות";
+$string['downloadreport'] = "דוח הורדות";
+
+$string['configurable_reports:manageownreports'] = "נהל דוחות אישיים";
+$string['configurable_reports:managereports'] = "נהל דוחות";
+$string['configurable_reports:managesqlreports'] = "SQL נהל דוחות";
+$string['configurable_reports:viewreports'] = "צפה בדוחות";
+
+$string['exportoptions'] = "ייצא אפשרויות";
+$string['field'] = "שדה";
+
+// Report form
+$string['typeofreport'] = "סוג הדוח";
+$string['enablejsordering'] = "JavaScript אפשר סידור";
+$string['enablejspagination'] = "JavaScript אפשר עימוד";
+$string['export_ods'] = "ODS ייצא בפורמט";
+$string['export_xls'] = "XLS ייצא בפורמט";
+$string['viewreport'] = "צפה בדוח";
+$string['norecordsfound'] = "לא נמצאו רשומות";
+
+// Columns
+$string['column'] = "עמודה";
+$string['nocolumnsyet'] = "עדיין אין עמודות";
+$string['tablealign'] = "יישור טבלה";
+$string['tablecellspacing'] = "ריווח מילים בטבלא - CellSpacing";
+$string['tablecellpadding'] = "ריווח תאים בטבלא - CellPadding";
+$string['tableclass'] = "Table class";
+$string['tablewidth'] = "רוחב הטבלה";
+$string['cellalign'] = "יישור התא";
+$string['cellwrap'] = "כריכת הטקסט בתא";
+$string['cellsize'] = "גודל התא";
+
+
+// Conditions
+$string['conditionexpr'] = "תנאים";
+$string['conditionexprhelp'] = "הכנס תא נכון : (c1 and c2) or (c4 and c3)";
+$string['noconditionsyet'] = "לא הוגדרו תנאים";
+$string['operator'] = "אופרטור";
+$string['value'] = "ערך";
+
+// Filter
+$string['filter'] = "פילטר";
+$string['nofilteryet'] = "לא הוגדרו פילטרים";
+$string['courses'] = "קורסים";
+$string['nofiltersyet'] = "לא הוגדרו פילטרים";
+
+// Calcs
+$string['nocalcsyet'] = "לא הוגדרו חישובים";
+
+// Plot
+$string['noplotyet'] = "לא הוגדר תרשים";
+
+// Permissions
+
+$string['nopermissionsyet'] = "לא הוגדרו הרשאות";
+
+// Ordering
+
+$string['noorderingyet'] = "לא הוגדר סדר";
+$string['userfieldorder'] = "סידור שדה משתמש";
+
+// Plugins
+$string['coursefield'] = "שדה קורס";
+$string['ccoursefield'] = "תנאי שדה קורס";
+$string['roleusersn'] = "מספר משתמשים בעלי תפקיד ...";
+$string['coursecategory'] = "קורסים בקטוגוריה";
+$string['filtercourses'] = "קורסים";
+$string['filtercourses_summary'] = "פילטר זה מציג רשימה של קורסים , ניתן לבחור רק קורס אחד באותו זמן";
+$string['roleincourse'] = "משתמש עם תפקיד בדוח הקורס הנוכחי";
+$string['reportscapabilities'] = "יכולות דיווח";
+$string['reportscapabilities_summary'] = "משתמשים שיכולת צפייה בדוחות מאופשרת";
+$string['sum'] = "סכום";
+$string['max'] = "מקסימום";
+$string['min'] = "מינימום";
+$string['average'] = "ממוצע";
+$string['pie'] = "דיאגרמת עוגה";
+$string['piesummary'] = "גרף דיאגרמת עוגה";
+$string['pieareaname'] = "שם";
+$string['pieareavalue'] = "ערך";
+$string['piesummary'] = "גרף דיאגרמת עוגה";
+
+$string['anyone'] = "כל אחד";
+$string['anyone_summary'] = "כל משתמש בקמפוס יוכל לצפות בדוח";
+
+$string['currentuserfinalgrade'] = "ציון סופי בקורס של המשתמש הנוכחי";
+
+$string['currentuserfinalgrade_summary'] = "עמודה  זו מראה את הציון הסופי של המשתמש הנוכחי בשורת הקורס";
+$string['userfield'] = "שדה פרופיל משתמש";
+
+$string['cuserfield'] = "תנאי שדה משתמש";
+$string['direction'] = "כיוון";
+
+$string['courseparent'] = "קורס שההורה שלו הוא";
+$string['coursechild'] = "קורסים שהם בנים של";
+
+$string['currentusercourses'] = "קורסים שהמשתמש הנוכחי רשום אליהם";
+$string['currentusercourses_summary'] = "רשימת קורסים של המשתמש הנוכחי";
+$string['currentreportcourse'] = "קורס של הדוח הנוכחי";
+$string['currentreportcourse_summary'] = "הקורס בו נוצר הדוח";
+
+$string['coursefieldorder'] = "סידור השדות בקורס";
+
+$string['fcoursefield'] = "פילטר שדות בקורס";
+$string['usersincoursereport'] = "משתמש כלשהו בדוח הקורס הנוכחי";
+
+$string['groupvalues'] = "ערכים זהים בקבוצה - סכום";
+$string['fuserfield'] = "פילטר שדה של משמש";
+
+$string['module'] = "מודול";
+
+$string['usersincurrentcourse'] = "משתמשים בקורס בדוח הנוכחי";
+$string['usersincurrentcourse_summary'] = "משתמשים עם התפקיד-ים הנבחר-ים בקורס המדווח";
+
+$string['usermodoutline'] = "מצב המתאר של מודול משתמש";
+$string['donotshowtime'] = "אל תציג תאריכים";
+$string['usermodactions'] = "פעולות מודול משתמש";
+
+$string['currentuser'] = "משתמש נוכחי";
+$string['currentuser_summary'] = "המשתמש הצופה בדוח";
+
+$string['puserfield'] = "ערך שדה משתמש";
+$string['puserfield_summary'] = "משתמש בעל ערך נבחר בשדה הנבחר";
+
+$string['startendtime'] = "פילטר תאריך התחלה-סיום";
+$string['starttime'] = "תאריך התחלה";
+$string['endtime'] = "תאריך סיום";
+
+$string['template'] = "תבנית";
+$string['availablemarks'] = "סימנים אפשריים";
+$string['header'] = "כותרת עליונה";
+$string['footer'] = "כותרת תחתונה";
+$string['templaterecord'] = "תבנית רשומה";
+$string['querysql'] = "SQL שאילתת";
+$string['filterstartendtime_summary'] = "פילטר תאריך התחלה-סיוםr";
+
+$string['pagination'] = "עימוד";
+$string['disabled'] = "מנוטרל";
+$string['enabled'] = "מאופשר";
+
+$string['reportcolumn'] = "עמודת דוח שונה";
+
+$string['reporttable'] = "טבלת דוח";
+$string['columnandcellproperties'] = "תכונות של עמודה ותא";
+$string['componenthelp'] = "עזרה למרכיב";
+
+$string['badsize'] = 'Incorrect size, it must be in &#37; or px';
+$string['badtablewidth'] = 'Incorrect width, it must be in &#37; or absolute value';
+$string['missingcolumn'] = "דרושה עמודה";
+$string['error_operator'] = "Operator not allowed";
+
+$string['error_field'] = "שדה לא מורשה";
+$string['error_value_expected_integer'] = "מצפה למספר שלם";
+$string['badconditionexpr'] = "ביטוי לא נכון לתנאי";
+
+$string['notallowedwords'] = "מילים לא מורשות";
+$string['nosemicolon'] = "אין נקודה פסיקn";
+$string['noexplicitprefix'] = "אין קידומת מפורשת";
+$string['queryfailed'] = "שאילתא נכשלה";
+$string['norowsreturned'] = "לא הוחזרו שורות";
+
+$string['listofsqlreports'] = '<a href=\"http://docs.moodle.org/en/ad-hoc_contributed_reports\" target=\"_blank\">List of SQL Contributed reports</a>';
+
+$string['usersincoursereport_summary'] = "משתמש כלשהו בדוח הקורס הנוכחי";
+
+$string['printreport'] = 'הדפס דוח';
+
+$string['importreport'] = "ייבא דווח";
+$string['exportreport'] = "ייצוא דוח";
+
+$string['download'] = "הורדה";
+
+$string['report_timeline'] = 'דוח לפי ציר זמן - Timeline';
+$string['timeline'] = 'ציר זמן';
+$string['timemode'] = 'מוד זמן';
+$string['previousdays'] = 'ימים קודמים';
+$string['fixeddate'] = 'תאריך קבוע';
+$string['previousstart'] = 'התחלה קודמת';
+$string['previousend'] = 'סיום קודם';
+$string['forcemidnight'] = 'הכרח חצות';
+$string['timeinterval'] = 'מרווח זמן';
+$string['date'] = 'תאריך';
+$string['dateformat'] = 'פורמט תאריך';
+$string['customdateformat'] = 'פורמט תאריך מותאם';
+$string['custom'] = 'התאם';
+
+$string['line'] = 'גרף קווי';
+$string['userstats'] = 'סטטיסטיקת משתמש';
+$string['stat'] = 'סטטיסטיקה';
+$string['statslogins'] = 'התחברות לפלטפורמה';
+$string['activityview'] = 'מראי פעילות';
+$string['activitypost'] = 'פרסום פעילות';
+$string[''] = '';
+$string['globalstatsshouldbeenabled'] = 'Site statistics must be enabled. Go to Admin -> Server -> Statistics';
+
+$string['xaxis'] = 'X ציר';
+$string['yaxis'] = 'Y ציר';
+$string['serieid'] = 'Serie column';
+$string['groupseries'] = 'Group series';
+$string['linesummary'] = 'A line graph with multiple series of data';
+
+$string['coursestats'] = 'סטטיסטיקה של קורס';
+$string['statstotalenrolments'] = 'סכום הנרשמים';
+$string['statsactiveenrolments'] = 'נרשמים פעילים - בשבוע שעבר';
+$string['youmustselectarole'] = 'לפחות תפקיד נדרש';
+
+$string['report_categories'] = 'דוח קטגוריות';
+$string['categoryfield'] = 'שדה קטגוריה';
+$string['categoryfieldorder'] = 'סדר שדה הקטגוריה';
+$string['categories'] = 'קטגוריות';
+$string['parentcategory'] = 'קטגוריית הורה';
+$string['filtercategories'] = 'קטגוריות פילטר';
+$string['filtercategories_summary'] = 'מיין לפי קטגוריה';
+
+$string['includesubcats'] = 'הכלל תת קטגוריות';
+
+$string['coursededicationtime'] = 'זמן השקעה בקורס';
+
+?>
