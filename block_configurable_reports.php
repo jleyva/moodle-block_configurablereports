f1ac9b72e8ff0bba1122508b4fa7ca63904d6862
Nadav Kavalerchik <nadavkav@gmail.com>
2013-09-09 00:48:20 +0300

    block_configurable_reports - syntax updates, translation token update, minor UI improvement

--- block_configurable_reports.php
+++ block_configurable_reports.php
@@ -21,52 +21,51 @@
   * @author: Juan leyva <http://www.twitter.com/jleyvadelgado>
   * @date: 2009
   */
-  
+
 class block_configurable_reports extends block_list {
-    
+
     /**
      * Sets the block name and version number
      *
      * @return void
      **/
-    function init() {
-        $this->title = get_string('blockname', 'block_configurable_reports');
-        $this->version = 2010060101;  // YYYYMMDDXX
+    public function init() {
+        $this->title = get_string('pluginname', 'block_configurable_reports');
     }
-   
+
     /**
      * Where to add the block
      *
      * @return boolean
      **/
-    function applicable_formats() {
+    public function applicable_formats() {
         return array('site' => true, 'course' => true);
     }
-    
+
     /**
      * Global Config?
      *
      * @return boolean
      **/
-    function has_config() {
-        return false;
+    public function has_config() {
+        return true;
     }
-    
+
     /**
      * More than one instance per page?
      *
      * @return boolean
      **/
-    function instance_allow_multiple() {
+    public function instance_allow_multiple() {
       return false;
-    } 
-	
+    }
+
     /**
      * Gets the contents of the block (course view)
      *
      * @return object An object with the contents
      **/
-    function get_content() {
+    public function get_content() {
         global $DB, $USER, $CFG ,$COURSE;
 
         if($this->content !== NULL) {
@@ -76,24 +75,24 @@ class block_configurable_reports extends block_list {
 		$this->content = new stdClass;
 		$this->content->footer = '';
 		$this->content->icons = array();
-		
+
 		if (!isloggedin())
 			return $this->content;
-		
+
 		require_once($CFG->dirroot."/blocks/configurable_reports/locallib.php");
-		
+
 		$course = $DB->get_record('course',array('id' => $COURSE->id));
-		
+
 		if(!$course)
 			print_error('coursedoesnotexists');
-		
+
 		if ($course->id == SITEID)
 			$context = get_context_instance(CONTEXT_SYSTEM);
 		else
 			$context = get_context_instance(CONTEXT_COURSE, $course->id);
 
 		$reports = $DB->get_records('block_configurable_reports',array('courseid' => $course->id),'name ASC');
-		
+
 		if($reports){
 			foreach($reports as $report){
 				if($report->visible && cr_check_report_permissions($report,$USER->id,$context)){
@@ -102,11 +101,11 @@ class block_configurable_reports extends block_list {
 				}
 			}
 		}
-		
+
 		if(has_capability('block/configurable_reports:managereports', $context) || has_capability('block/configurable_reports:manageownreports', $context)){
-			$this->content->items[] = '<a href="'.$CFG->wwwroot.'/blocks/configurable_reports/managereport.php?courseid='.$course->id.'">'.(get_string('managereports','block_configurable_reports')).'</a>';
+			$this->content->items[] = '<hr/><a href="'.$CFG->wwwroot.'/blocks/configurable_reports/managereport.php?courseid='.$course->id.'">'.(get_string('managereports','block_configurable_reports')).'</a>';
 		}
-				
+
         return $this->content;
     }
 }
