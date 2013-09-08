ec04a6e5de4ed5cf8569a6710b64468b3f40e52a
Nadav Kavalerchik <nadavkav@gmail.com>
2013-09-09 00:56:49 +0300

    version update (file clean up)

--- version.php
+++ version.php
@@ -1,6 +1,38 @@
 <?php
-    $plugin->version = 2011040105;
-    // Moodle version required
-    $plugin->requires = 2010112400;
-    $plugin->maturity = MATURITY_STABLE;
-    $plugin->release = '2.2';
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * Configurable Reports - A Moodle block for creating customizable reports
+ *
+ * @package     block_configurable_reports
+ * @author:     Juan leyva <http://www.twitter.com/jleyvadelgado>
+ * @date:       2013-09-07
+ *
+ * @copyright  Juan leyva <http://www.twitter.com/jleyvadelgado>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2013090706;
+// Moodle version required
+$plugin->requires = 2010112400;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '2.2';
+$plugin->component = 'block_configurable_reports'; // Full name of the plugin (used for diagnostics)
