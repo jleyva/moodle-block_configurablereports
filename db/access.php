406f24b5d9013587d5cd6085982c2b7282e12b62
Nadav Kavalerchik <nadavkav@gmail.com>
2013-09-23 18:10:06 +0300

    Add GNU General Public License (access.php)

--- db/access.php
+++ db/access.php
@@ -1,5 +1,33 @@
 <?php
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * Configurable Reports - A Moodle block for creating customizable reports
+ *
+ * @package     block_configurable_reports
+ * @author:     Juan leyva <http://www.twitter.com/jleyvadelgado>
+ * @date:       2013-09-07
+ *
+ * @copyright  Juan leyva <http://www.twitter.com/jleyvadelgado>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $capabilities = array(
 
     'block/configurable_reports:addinstance' => array(
@@ -31,8 +59,8 @@ $capabilities = array(
         'archetypes' => array(
             'manager' => CAP_ALLOW
         )
-    ),	
-	
+    ),
+
     'block/configurable_reports:manageownreports' => array(
 
         'captype' => 'write',
