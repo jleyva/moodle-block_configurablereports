6053983fc0ad24812a75dd2009e66e8130837a10
Nadav Kavalerchik <nadavkav@gmail.com>
2013-09-23 18:06:15 +0300

    remove "localhost" default URL setting

--- settings.php
+++ settings.php
@@ -4,7 +4,7 @@ defined('MOODLE_INTERNAL') || die;
 
 if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configtext('block_configurable_reports/dbhost', get_string('dbhost', 'block_configurable_reports'),
-                    get_string('dbhostinfo', 'block_configurable_reports'), 'localhost', PARAM_URL, 30));
+                    get_string('dbhostinfo', 'block_configurable_reports'), '', PARAM_URL, 30));
     $settings->add(new admin_setting_configtext('block_configurable_reports/dbname', get_string('dbname', 'block_configurable_reports'),
                     get_string('dbnameinfo', 'block_configurable_reports'), '', PARAM_RAW, 30));
     $settings->add(new admin_setting_configpasswordunmask('block_configurable_reports/dbuser', get_string('dbuser', 'block_configurable_reports'),
