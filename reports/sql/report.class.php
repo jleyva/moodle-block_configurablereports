46401a14d916271afdd3f65bca2d5b25ae177e09
Nadav Kavalerchik <nadavkav@gmail.com>
2013-12-09 23:52:15 +0200

    New feature - run report in context of course category %%CATEGORYID%%

--- reports/sql/report.class.php
+++ reports/sql/report.class.php
@@ -46,6 +46,7 @@ class report_sql extends report_base {
 
         $sql = str_replace('%%USERID%%', $USER->id, $sql);
         $sql = str_replace('%%COURSEID%%', $COURSE->id, $sql);
+        $sql = str_replace('%%CATEGORYID%%', $COURSE->category, $sql);
 
 		// See http://en.wikipedia.org/wiki/Year_2038_problem
 		$sql = str_replace(array('%%STARTTIME%%','%%ENDTIME%%'),array('0','2145938400'),$sql);
