583eb6d50a4f94765957ab4edf472cbbf72f8ffd
Juan Leyva <juanleyvadelgado@gmail.com>
2013-12-12 10:01:10 +0100

    Fixing invalid course filtering
    Conflicts:
    	editreport_form.php
    	filter_form.php
    	import_form.php

--- components/columns/userstats/plugin.class.php
+++ components/columns/userstats/plugin.class.php
@@ -20,7 +20,7 @@
   * @package blocks
   * @author: Juan leyva <http://www.twitter.com/jleyvadelgado>
   * @date: 2009
-  */ 
+  */
 
 require_once($CFG->dirroot.'/blocks/configurable_reports/plugin.class.php');
 
@@ -32,58 +32,58 @@ class plugin_userstats extends plugin_base{
         $this->form = true;
         $this->reporttypes = array('users');
     }
-    
+
     function summary($data){
         return format_string($data->columname);
     }
-    
+
     function colformat($data){
         $align = (isset($data->align))? $data->align : '';
         $size = (isset($data->size))? $data->size : '';
         $wrap = (isset($data->wrap))? $data->wrap : '';
         return array($align,$size,$wrap);
     }
-    
+
     // data -> Plugin configuration data
     // row -> Complet user row c->id, c->fullname, etc...
     function execute($data,$row,$user,$courseid,$starttime=0,$endtime=0){
         global $DB, $CFG;
-        
+
         $stat = '--';
-                    
+
         $filter_starttime = optional_param('filter_starttime',0,PARAM_RAW);
         $filter_endtime = optional_param('filter_endtime',0,PARAM_RAW);
-        
+
         // Do not apply filters in timeline report (filters yet applied)
         if($starttime && $endtime){
             $filter_starttime = 0;
             $filter_endtime = 0;
         }
-        
+
         if($filter_starttime and $filter_endtime){
             $filter_starttime = make_timestamp($filter_starttime['year'],$filter_starttime['month'],$filter_starttime['day']);
             $filter_endtime = make_timestamp($filter_endtime['year'],$filter_endtime['month'],$filter_endtime['day']);
         }
-        
+
         $starttime = ($filter_starttime)? $filter_starttime : $starttime;
         $endtime = ($filter_endtime)? $filter_endtime : $endtime;
-        
+
         if($data->stat == 'coursededicationtime'){
             $sql = "userid = ?";
             $params =  array($row->id);
-            
+
             if($courseid != 1){
                 $sql .= " AND course = ?";
                 $params = array_merge($params, array($courseid));
             }
-                
+
             if($starttime and $endtime){
                 $starttime = usergetmidnight($starttime) + 24*60*60;
                 $endtime = usergetmidnight($endtime) + 24*60*60;
                 $sql .= " AND time >= ? AND time <= ?";
                 $params = array_merge($params, array($starttime, $endtime));
-            }    
-                                    
+            }
+
             // Code from Course Dedication Block
             if ($logs = $DB->get_records_select("log", $sql, $params, "time ASC", "id,time")) {
                 // This should be a config value in some where
@@ -91,12 +91,12 @@ class plugin_userstats extends plugin_base{
                 $previouslog = array_shift($logs);
                 $previouslogtime = $previouslog->time;
                 $sessionstart = $previouslogtime;
-                $totaldedication = 0;             
+                $totaldedication = 0;
 
                 foreach ($logs as $log) {
-                    if (($log->time - $previouslogtime) > $limitinseconds) {                        
+                    if (($log->time - $previouslogtime) > $limitinseconds) {
                         $dedication = $previouslogtime - $sessionstart;
-                        $totaldedication += $dedication;                        
+                        $totaldedication += $dedication;
                         $sessionstart = $log->time;
                     }
                     $previouslogtime = $log->time;
@@ -112,8 +112,8 @@ class plugin_userstats extends plugin_base{
             }
             // Code from Course Dedication Block
             return 0;
-        }        
-        
+        }
+
         switch($data->stat){
             case 'activityview':
                                 $total = 'statsreads';
@@ -123,34 +123,34 @@ class plugin_userstats extends plugin_base{
                                 $total = 'statswrites';
                                 $stattype = 'activity';
                                 break;
-            case 'logins':            
+            case 'logins':
             default:
                                 $total = 'statsreads';
                                 $stattype = 'logins';
         }
         $sql = "SELECT SUM($total) as total FROM {stats_user_daily} WHERE stattype = ? AND userid = ?";
         $params = array($stattype,$row->id);
-        
-        if ($courseid != SITEID) {
+
+        if ($courseid != SITEID and $data->stat != 'logins') {
             $sql .= " AND courseid = ?";
             $params[] = $courseid;
         }
-        
+
         if($starttime and $endtime){
             $starttime = usergetmidnight($starttime) + 24*60*60;
             $endtime = usergetmidnight($endtime) + 24*60*60;
             $sql .= " AND timeend >= $starttime AND timeend <= $endtime";
             $params = array_merge($params, array($starttime,$endtime));
         }
-        
-        if($res = $DB->get_records_sql($sql, $params)){                
+
+        if($res = $DB->get_records_sql($sql, $params)){
             $res = array_shift($res);
             if($res->total != NULL)
                 return $res->total;
             else
                 return 0;
-        }        
-        
+        }
+
         return $stat;
-    }    
+    }
 }
