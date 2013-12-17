fa6577de44d4a77abe41d6c2b69129c8804b1985
Juan Leyva <juanleyvadelgado@gmail.com>
2013-12-17 13:49:56 +0100

    Fixing notices & warnings

--- components/columns/reportcolumn/plugin.class.php
+++ components/columns/reportcolumn/plugin.class.php
@@ -20,7 +20,7 @@
   * @package blocks
   * @author: Juan leyva <http://www.twitter.com/jleyvadelgado>
   * @date: 2009
-  */ 
+  */
 
 require_once($CFG->dirroot.'/blocks/configurable_reports/plugin.class.php');
 
@@ -34,24 +34,24 @@ class plugin_reportcolumn extends plugin_base{
 		$this->form = true;
 		$this->reporttypes = array('courses','users','timeline','categories');
 	}
-	
-	function summary($data){		
+
+	function summary($data){
 		return format_string($data->columname);
 	}
-	
-	
+
+
 	function colformat($data){
 		$align = (isset($data->align))? $data->align : '';
 		$size = (isset($data->size))? $data->size : '';
 		$wrap = (isset($data->wrap))? $data->wrap : '';
 		return array($align,$size,$wrap);
-	}	
-	
+	}
+
 	function get_user_reports(){
 		global $DB, $USER;
-		
+
 		$supported = array('courses'=>array('users'), 'users'=>array('courses'), 'timeline'=>array('users','courses','sql'), 'categories' => array('courses'));
-		
+
 		$reports = cr_get_my_reports($this->report->courseid, $USER->id);
 		if($reports){
 			foreach($reports as $key=>$val){
@@ -61,10 +61,10 @@ class plugin_reportcolumn extends plugin_base{
 		}
 		return $reports;
 	}
-	
+
 	function get_current_report($report){
 		$components = cr_unserialize($report->components);
-			
+
 		if(!is_array($components) || empty($components['columns']['elements']))
 			return false;
 
@@ -75,16 +75,16 @@ class plugin_reportcolumn extends plugin_base{
 		}
 		return 0;
 	}
-	
+
 	function get_report_columns($reportid){
 		global $DB;
-		
+
 		$columns = array();
 		if(! $report = $DB->get_record('block_configurable_reports',array('id' => $reportid)))
 			return $columns;
-			
+
 		$components = cr_unserialize($report->components);
-			
+
 		if(!is_array($components) || empty($components['columns']['elements']))
 			return $columns;
 
@@ -92,10 +92,10 @@ class plugin_reportcolumn extends plugin_base{
 		foreach($elements as $e){
 			$columns[] = $e['summary'];
 		}
-		
+
 		return $columns;
 	}
-	
+
 	function fix_condition_expr($condition,$count){
 		switch($count){
 			case 0: return '';
@@ -104,24 +104,24 @@ class plugin_reportcolumn extends plugin_base{
 			default: return $condition." and c$count";
 		}
 	}
-	
+
 	// data -> Plugin configuration data
 	// row -> Complet course/user row c->id, c->fullname, etc...
 	function execute($data,$row,$user,$courseid,$starttime=0,$endtime=0){
 		global $DB, $CFG;
-		
+
 		if(! $report = $DB->get_record('block_configurable_reports',array('id' => $data->reportid)))
 			print_error('reportdoesnotexists','block_configurable_reports');
-		
+
 		require_once($CFG->dirroot.'/blocks/configurable_reports/report.class.php');
 		require_once($CFG->dirroot.'/blocks/configurable_reports/reports/'.$report->type.'/report.class.php');
-	
+
 		if(!isset($this->reportcache[$row->id])){
-	
-			$reportclassname = 'report_'.$report->type;	
+
+			$reportclassname = 'report_'.$report->type;
 			$reportclass = new $reportclassname($report);
-			
-			// Delete conditions - TODO		
+
+			// Delete conditions - TODO
 			// Add new condition
 			// User report -> New condition "User courses"
 			// Course report -> New condition "Course users"
@@ -129,11 +129,11 @@ class plugin_reportcolumn extends plugin_base{
 				$reportclass->currentuser = $row;
 				$reportclass->starttime = $starttime;
 				$reportclass->endtime = $endtime;
-				
+
 				if($report->type == 'courses'){
 					$components = cr_unserialize($reportclass->config->components);
 					$newplugin = array('pluginname'=>'currentusercourses','fullname'=>'currentusercourses','formdata'=>new stdclass);
-					
+
 					$components['conditions']['elements'][] = $newplugin;
 					$components['conditions']['config']->conditionexpr = $this->fix_condition_expr($components['conditions']['config']->conditionexpr, count($components['conditions']['elements']));
 					$reportclass->config->components = cr_serialize($components);
@@ -143,57 +143,59 @@ class plugin_reportcolumn extends plugin_base{
 				$reportclass->currentcourseid = $row->id;
 				$reportclass->starttime = $starttime;
 				$reportclass->endtime = $endtime;
-				
-				if($report->type == 'users'){				
+
+				if($report->type == 'users'){
 					$components = cr_unserialize($reportclass->config->components);
-					
+
 					$roles = $DB->get_records('role');
 					$rolesid = array_keys($roles);
-					
+
 					$formdata = new stdclass;
 					$formdata->roles = $rolesid;
 					$newplugin = array('pluginname'=>'usersincurrentcourse','fullname'=>'usersincurrentcourse','formdata'=>$formdata);
-					
+
 					$components['conditions']['elements'][] = $newplugin;
-					$components['conditions']['config']->conditionexpr = $this->fix_condition_expr($components['conditions']['config']->conditionexpr, count($components['conditions']['elements']));
+					if (!empty($components['conditions']['config'])) {
+						$components['conditions']['config']->conditionexpr = $this->fix_condition_expr($components['conditions']['config']->conditionexpr, count($components['conditions']['elements']));
+					}
 					$reportclass->config->components = cr_serialize($components);
 				}
-			}			
+			}
 			else if($this->report->type == 'timeline'){
 				$reportclass->starttime = $row->starttime;
 				$reportclass->endtime = $row->endtime;
 			}
-			else if($this->report->type == 'categories'){				
+			else if($this->report->type == 'categories'){
 				$reportclass->starttime = $starttime;
 				$reportclass->endtime = $endtime;
-				
-				if($report->type == 'courses'){				
+
+				if($report->type == 'courses'){
 					$components = cr_unserialize($reportclass->config->components);
-					
+
 					$formdata = new stdclass;
 					$formdata->categoryid = $row->id;
 					$newplugin = array('pluginname'=>'coursecategory','fullname'=>'coursecategory','formdata'=>$formdata);
-					
+
 					$components['conditions']['elements'][] = $newplugin;
 					$components['conditions']['config']->conditionexpr = $this->fix_condition_expr($components['conditions']['config']->conditionexpr, count($components['conditions']['elements']));
 					$reportclass->config->components = cr_serialize($components);
 				}
-			}	
-		
+			}
+
 			$reportclass->create_report();
 			$this->reportcache[$row->id] = $reportclass->finalreport->table->data;
 		}
-				
+
 		if(!empty($this->reportcache[$row->id])){
 			$subtable = array();
 			foreach($this->reportcache[$row->id] as $r){
-				$subtable[] = $r[$data->column];				
-			}			
+				$subtable[] = $r[$data->column];
+			}
 			return $subtable;
 		}
-		
+
 		return '';
 	}
-	
+
 }
 
