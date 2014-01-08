3b2cad6c8bc68d6395bf5817c42a2ac95777ec1b
Juan Leyva <juanleyvadelgado@gmail.com>
2014-01-08 10:17:23 +0100

    Fixing CONTRIB-4824 and polisihing code style

--- block_configurable_reports.php
+++ block_configurable_reports.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -16,11 +15,11 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /** Configurable Reports
-  * A Moodle block for creating customizable reports
-  * @package blocks
-  * @author: Juan leyva <http://www.twitter.com/jleyvadelgado>
-  * @date: 2009
-  */
+ * A Moodle block for creating customizable reports
+ * @package blocks
+ * @author: Juan leyva <http://www.twitter.com/jleyvadelgado>
+ * @date: 2009
+ */
 
 class block_configurable_reports extends block_list {
 
@@ -69,7 +68,7 @@ class block_configurable_reports extends block_list {
      * @return boolean
      **/
     public function instance_allow_multiple() {
-      return false;
+        return false;
     }
 
     /**
@@ -78,56 +77,41 @@ class block_configurable_reports extends block_list {
      * @return object An object with the contents
      **/
     public function get_content() {
-        global $DB, $USER, $CFG ,$COURSE;
+        global $DB, $USER, $CFG, $COURSE;
 
-        if($this->content !== NULL) {
+        if ($this->content !== null) {
             return $this->content;
         }
 
-    		$this->content = new stdClass;
-    		$this->content->footer = '';
-    		$this->content->icons = array();
+        $this->content = new stdClass;
+        $this->content->footer = '';
+        $this->content->icons = array();
 
-    		if (!isloggedin())
-    			return $this->content;
-
-    		require_once($CFG->dirroot."/blocks/configurable_reports/locallib.php");
-
-    		$course = $DB->get_record('course',array('id' => $COURSE->id));
-
-    		if(!$course)
-    			print_error('coursedoesnotexists');
-
-    		if ($course->id == SITEID) {
-                $context = context_system::instance();
-            } else {
-                $context = context_course::instance($course->id);
-            }
-
-        // Site (Shared) reports
-        if (!empty($this->config->displayglobalreports)) {
-          $reports = $DB->get_records('block_configurable_reports',array('global' => 1),'name ASC');
-
-          if ($reports) {
-              foreach($reports as $report){
-                  if($report->visible && cr_check_report_permissions($report, $USER->id, $context)){
-                      $rname = format_string($report->name);
-                      $this->content->items[] = '<a href= "'.$CFG->wwwroot.'/blocks/configurable_reports/viewreport.php?id='.$report->id.'&courseid='.$course->id.'" alt="'.$rname.'">'.$rname.'</a>';
-                  }
-              }
-              $this->content->items[] = '========';
-          }
+        if (!isloggedin()) {
+            return $this->content;
         }
 
-        // Course reports
-        if (!property_exists($this, 'config')
-            or !property_exists($this->config, 'displayreportslist')
-            or $this->config->displayreportslist) {
-            $reports = $DB->get_records('block_configurable_reports',array('courseid' => $course->id),'name ASC');
+        require_once($CFG->dirroot."/blocks/configurable_reports/locallib.php");
+
+        $course = $DB->get_record('course', array('id' => $COURSE->id));
+
+        if (!$course) {
+            print_error('coursedoesnotexists');
+        }
+
+        if ($course->id == SITEID) {
+            $context = context_system::instance();
+        } else {
+            $context = context_course::instance($course->id);
+        }
+
+        // Site (Shared) reports.
+        if (!empty($this->config->displayglobalreports)) {
+            $reports = $DB->get_records('block_configurable_reports', array('global' => 1), 'name ASC');
 
             if ($reports) {
-                foreach($reports as $report) {
-                    if(!$report->global && $report->visible && cr_check_report_permissions($report,$USER->id,$context)){
+                foreach ($reports as $report) {
+                    if ($report->visible && cr_check_report_permissions($report, $USER->id, $context)) {
                         $rname = format_string($report->name);
                         $this->content->items[] = '<a href= "'.$CFG->wwwroot.'/blocks/configurable_reports/viewreport.php?id='.$report->id.'&courseid='.$course->id.'" alt="'.$rname.'">'.$rname.'</a>';
                     }
@@ -136,9 +120,27 @@ class block_configurable_reports extends block_list {
             }
         }
 
-    		if(has_capability('block/configurable_reports:managereports', $context) || has_capability('block/configurable_reports:manageownreports', $context)){
-    			$this->content->items[] = '<a href="'.$CFG->wwwroot.'/blocks/configurable_reports/managereport.php?courseid='.$course->id.'">'.(get_string('managereports','block_configurable_reports')).'</a>';
-    		}
+        // Course reports.
+        if (!property_exists($this, 'config')
+            or !isset($this->config->displayreportslist)
+            or $this->config->displayreportslist) {
+            $reports = $DB->get_records('block_configurable_reports', array('courseid' => $course->id), 'name ASC');
+
+            if ($reports) {
+                foreach ($reports as $report) {
+                    if (!$report->global && $report->visible && cr_check_report_permissions($report, $USER->id, $context)) {
+                        $rname = format_string($report->name);
+                        $this->content->items[] = '<a href= "'.$CFG->wwwroot.'/blocks/configurable_reports/viewreport.php?id='.$report->id.'&courseid='.$course->id.'" alt="'.$rname.'">'.$rname.'</a>';
+                    }
+                }
+                $this->content->items[] = '========';
+            }
+        }
+
+            if (has_capability('block/configurable_reports:managereports', $context)
+                || has_capability('block/configurable_reports:manageownreports', $context)) {
+                $this->content->items[] = '<a href="'.$CFG->wwwroot.'/blocks/configurable_reports/managereport.php?courseid='.$course->id.'">'.(get_string('managereports', 'block_configurable_reports')).'</a>';
+            }
 
         return $this->content;
     }
@@ -146,47 +148,51 @@ class block_configurable_reports extends block_list {
     public function cron() {
         global $CFG, $DB;
 
-        $hour = get_config('block_configurable_reports','cron_hour');
-        $min = get_config('block_configurable_reports','cron_minute');
+        $hour = get_config('block_configurable_reports', 'cron_hour');
+        $min = get_config('block_configurable_reports', 'cron_minute');
 
         $date = usergetdate(time());
         $usertime = mktime($date['hours'], $date['minutes'], $date['seconds'], $date['mon'], $date['mday'], $date['year']);
 
         $crontime = mktime($hour, $min, $date['seconds'], $date['mon'], $date['mday'], $date['year']);
 
-        if ( ($crontime - $usertime) < 0 ) return false;
+        if ( ($crontime - $usertime) < 0 ) {
+            return false;
+        }
 
         $lastcron = $DB->get_field('blocks', 'lastcron', array('name' => 'configurable_reports'));
-        if (!$lastcron and ($lastcron + $this->cron < time()) ) return false;
+        if (!$lastcron and ($lastcron + $this->cron < time()) ) {
+            return false;
+        }
 
         // Starting to run...
-        //$DB->set_field('blocks','lastcron',time(), array('name' => 'configurable_reports'));
+        //$DB->set_field('blocks', 'lastcron',time(), array('name' => 'configurable_reports'));
 
         require_once($CFG->dirroot."/blocks/configurable_reports/locallib.php");
         require_once($CFG->dirroot.'/blocks/configurable_reports/report.class.php');
         require_once($CFG->dirroot.'/blocks/configurable_reports/reports/sql/report.class.php');
 
-        //mtrace("\nConfigurable report (block)");
+        mtrace("\nConfigurable report (block)");
 
         $reports = $DB->get_records('block_configurable_reports');
         if ($reports) {
             foreach ($reports as $report) {
-                // Running only SQL reports. $report->type == 'sql'
+                // Running only SQL reports. $report->type == 'sql'.
                 if ($report->type == 'sql' AND (!empty($report->cron) AND $report->cron == '1')) {
                     $reportclass = new report_sql($report);
 
-                    // Execute it using $remoteDB
+                    // Execute it using $remoteDB.
                     $starttime = microtime(true);
                     mtrace("\nExecuting query '$report->name'");
-                    //$results = $reportclass->create_report();
+
                     $components = cr_unserialize($reportclass->config->components);
                     $config = (isset($components['customsql']['config']))? $components['customsql']['config'] : new stdclass;
                     $sql = $reportclass->prepare_sql($config->querysql);
-                    //if (strpos($sql, ';') !== false) {
-                        $sqlqueries = explode(';',$sql);
-                    //} else
+
+                    $sqlqueries = explode(';', $sql);
+
                     foreach ($sqlqueries as $sql) {
-                        mtrace(substr($sql,0,60)); // Show some SQL
+                        mtrace(substr($sql, 0, 60)); // Show some SQL.
                         $results = $reportclass->execute_query($sql);
                         mtrace(($results==1) ? '...OK time='.round((microtime(true) - $starttime) * 1000).'mSec' : 'Some SQL Error'.'\n');
                     }
