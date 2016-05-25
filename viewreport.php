4612e42734726eb7b457923405a73b7e0d26fa3c
Juan Leyva <juanleyvadelgado@gmail.com>
2016-05-25 09:40:30 +0200

    Fix #69 (query broken)

--- viewreport.php
+++ viewreport.php
@@ -66,6 +66,7 @@ if (!$reportclass->check_permissions($USER->id, $context)) {
 $PAGE->set_context($context);
 $PAGE->set_pagelayout('incourse');
 $PAGE->set_url('/blocks/configurable_reports/viewreport.php', ['id' => $id]);
+$PAGE->requires->jquery();
 
 $reportclass->create_report();
 
