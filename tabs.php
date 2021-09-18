78825c5ebbf65524586e81adec700fd7360ea7e8
Christian Abila <christian@abila.at>
2021-09-18 19:55:03 +0200

    fix indentation of phpdoc tags contents

--- tabs.php
+++ tabs.php
@@ -18,8 +18,8 @@
  * Configurable Reports
  * 
  * A Moodle block for creating customizable reports
- * @package block_configurable_reports
- * @author Juan leyva <http://www.twitter.com/jleyvadelgado>
+ * @package     block_configurable_reports
+ * @author      Juan leyva <http://www.twitter.com/jleyvadelgado>
  * @copyright   Juan leyva <http://www.twitter.com/jleyvadelgado>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
